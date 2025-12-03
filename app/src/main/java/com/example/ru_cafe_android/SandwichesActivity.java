0e2e61ce3db6f76692f795bb419f9b3927b1c482
Jay Suchak <jaysuchak33@gmail.com>
2025-12-03 00:28:40 -0500

    Implement sandwiches frontend

--- app/src/main/java/com/example/ru_cafe_android/SandwichesActivity.java
+++ app/src/main/java/com/example/ru_cafe_android/SandwichesActivity.java
@@ -1,5 +1,8 @@
 package com.example.ru_cafe_android;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -7,4 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
  * @author Namulun, modified Nov. 30, 2025
  */
 public class SandwichesActivity extends AppCompatActivity {
+
+    /**
+     * Sets up base elements for sandwiches page
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.sandwiches_activity);
+    }
 }
