f1f12590bbed6fddc34431af4cba87ff570bc8cf
Jay Suchak <jaysuchak33@gmail.com>
2025-12-04 11:53:51 -0500

    Implement singleton design for globalized order

---
+++ app/src/main/java/com/example/ru_cafe_android/OrderManager.java
@@ -0,0 +1,42 @@
+package com.example.ru_cafe_android;
+
+import com.example.ru_cafe_android.system.Order;
+
+/**
+ * Singleton order manager to prevent multiple global order instances
+ * @author Jay, modified 12/4/2025
+ */
+public final class OrderManager {
+    private static OrderManager instance;
+    private Order currentOrder;
+
+    /**
+     * Private constructor prevents instantiation outside of this file
+     */
+    private OrderManager() {
+    }
+
+    /**
+     * Creates an instance if one was not made, returns one if it was
+     * @return the only instance of this class
+     */
+    public static synchronized OrderManager getInstance() {
+        if (instance == null) instance = new OrderManager();
+        return instance;
+    }
+
+    /**
+     * Sets the centralized order
+     * @param order Order object to set to global
+     */
+    public void setCurrentOrder(Order order) { this.currentOrder = order; }
+
+    /**
+     * Retrieves the current global order
+     * @return Global order object
+     */
+    public Order getCurrrentOrder() { return this.currentOrder; }
+
+
+
+}
